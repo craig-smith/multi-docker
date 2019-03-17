5fd51439a51d05c85b8224f4538ab5fe087c222e
Craig Smith <poland_5@msn.com>
2019-03-17 08:35:51 +0100

    modified app.js

--- client/src/App.js
+++ client/src/App.js
@@ -20,6 +20,9 @@ class App extends Component {
             <Route exact path="/" component={Fib} />
             <Route path="/otherpage" component={OtherPage} />
           </div>
+          <div>
+            <h1>YO New Update</h1>
+          </div>
         </div>
       </Router>
     );
